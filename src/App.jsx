4566f99c8b449efa319a0a05d4a74e2a5d4065f7
ako2223 <akoval22@proton.me>
2024-03-19 12:19:40 +0600

    doing work, looks like something

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const getTasks = async () => {
+    const response = await axios.get("http://localhost:8000/todos");
+    const data = await response.data;
+    setTasks(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        className="img"
+        src="https://cdn.shopify.com/s/files/1/0603/3745/5243/files/1._Various_coloured_crayons_standing_upwards_with_purple_labels..jpg?v=1679538490"
+        alt=""
+      />
+      <h1>toTooooDoooos</h1>
+      <input type="text" />
+      <TaskList />
     </div>
   );
 }
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,12 @@
+import "./TaskList.css";
+
+function TaskList() {
+  return (
+    <div className="list">
+    
+      <input type="checkbox" />
+    </div>
+  );
+}
+
+export default TaskList;
